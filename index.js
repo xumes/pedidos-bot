4ae4c4aff936218ac6895f2a5e0eb3153b74ad05
Reginaldo Santos <reginaldo.santos@proposify.com>
2020-05-15 18:51:04 -0300

    adicionando o body parser

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+
 const app = express( );
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 
 app.get('/', (req, res) => {
