296251d35f477e9e4e8680e608e25044305b506c
Reginaldo Marcelo dos Santos <reginaldosantos.br@gmail.com>
2020-06-01 20:09:56 -0300

    criando a rota webhook (#14)

--- index.js
+++ index.js
@@ -3,39 +3,18 @@ const bodyParser = require("body-parser");
 
 const app = express( );
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json())
 
 
 app.get('/', (req, res) => {
   res.send("Olá Chatbot");
 })
 
-app.get('/pergunta', (req, res) => {
-  msg=req.query.pergunta;
-  res.send("você perguntou: " + msg);
-})
+app.post('/webhook', (req, res) => {
+  console.log("Cheguei no webhook");
 
-app.get('/mensagem/:tipo/:id', (req, res) => {
-  msg=req.params.tipo;
-  cod=req.params.id;
-  res.send("você quer editar o id #" + cod);
-})
-
-app.post('/pedido', (req, res) => {
-  console.log(req);
-  const produto = req.body.produto;
-  const qtd = req.body.quantidade;
-  const pagto = req.body.tipoPagamento;
-  const bebida = req.body.bebida;
-
-  const pedido = {
-    produto,
-    qtd,
-    pagto,
-    bebida
-  }
-
-  res.json(pedido);
+  console.log("body", req.body.queryResult);
 })
 
 const porta = process.env.PORT || 3000;
@@ -44,3 +23,41 @@ const hostname = "127.0.0.1"
 app.listen(porta, () => {
   console.log(`servidor rodando em http://${hostname}:${porta}`);
 })
+
+
+
+
+
+
+
+
+
+
+
+// app.get('/pergunta', (req, res) => {
+//   msg=req.query.pergunta;
+//   res.send("você perguntou: " + msg);
+// })
+
+// app.get('/mensagem/:tipo/:id', (req, res) => {
+//   msg=req.params.tipo;
+//   cod=req.params.id;
+//   res.send("você quer editar o id #" + cod);
+// })
+
+// app.post('/pedido', (req, res) => {
+//   console.log(req);
+//   const produto = req.body.produto;
+//   const qtd = req.body.quantidade;
+//   const pagto = req.body.tipoPagamento;
+//   const bebida = req.body.bebida;
+
+//   const pedido = {
+//     produto,
+//     qtd,
+//     pagto,
+//     bebida
+//   }
+
+//   res.json(pedido);
+// })
