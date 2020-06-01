1365b66fed980ce3f6c045c09b0535a96900c054
Reginaldo Marcelo dos Santos <reginaldosantos.br@gmail.com>
2020-06-01 16:33:57 -0300

    Testando (#11)
    * Alterando a porta do servidor
    
    * Corrigindo erro no hostname
    
    * Criando script Start
    
    * testando webhook
    
    * mais logs
    
    * mais logs
    
    * mais logs
    
    * testando o body

--- index.js
+++ index.js
@@ -41,9 +41,13 @@ app.post('/pedido', (req, res) => {
 app.post("/webhook", (req,res) => {
   console.log("received a post request");
 
-  console.log("queryResult", req.body.queryResult)
-  console.log("body", req.body.intent.displayName)
-  console.log("body", req.body.queryResult.queryText)
+  var speech =
+  req.body.queryResult &&
+  req.body.queryResult.parameters &&
+  req.body.queryResult.parameters.echoText
+    ? req.body.queryResult.parameters.echoText
+    : "Seems like some problem. Speak again.";
+
 
   const resposta = {
     "fulfillmentText": " ",
