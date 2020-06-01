317f47b84449ed7f1d16dcc8a40827e9389d3d1d
Reginaldo Santos <reginaldo.santos@proposify.com>
2020-06-01 16:16:32 -0300

    mais logs

--- index.js
+++ index.js
@@ -40,7 +40,9 @@ app.post('/pedido', (req, res) => {
 
 app.post("/webhook", (req,res) => {
   console.log("received a post request");
-  console.log("body", req)
+  console.log("queryResult", req.body.queryResult)
+  console.log("body", req.body.intent.displayName)
+  console.log("body", req.body.queryResult.queryText)
 
   const resposta = {
     "fulfillmentText": " ",
